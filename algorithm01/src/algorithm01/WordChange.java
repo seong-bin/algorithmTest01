0f02e7f84c1d94303017bd348022fb0b96f56de9
seong-bin <bsb4011@naver.com>
2021-05-18 16:24:07 +0900

    알고리즘 연습

--- algorithm01/src/algorithm01/WordChange.java
+++ algorithm01/src/algorithm01/WordChange.java
@@ -1,8 +1,9 @@
 package algorithm01;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 // 프로그래머스 단어 변환
 //두 개의 단어 begin, target과 단어의 집합 words가 있습니다. 아래와 같은 규칙을 이용하여 begin에서 target으로 변환하는 가장 짧은 변환 과정을 찾으려고 합니다.
@@ -19,36 +20,121 @@ import java.util.List;
 	//변환할 수 없는 경우에는 0를 return 합니다.
 public class WordChange {
 
+//    private static List<String> changeStr = new ArrayList<>();
+//    private static List<String> remainWord = new ArrayList<>();
 	public static void main(String[] args) {
-		int solution = solution("hit","cog", new String[] {"hot", "dot", "dog", "lot", "log", "cog"});
-		System.out.println("변활할 수 있는 단어의 개수 = "+solution);
+//		int solution = solution("hit","cog", new String[] {"hot", "dot", "dog", "lot","oit", "log", "cog"});
+//		int solution = solution("1234567000","1234567899", new String[] {"1234567800", "1234567890", "1234567899"});
+//		int solution = solution("hit","cog", new String[] {"hot", "dot", "dog", "lot", "log"});
+		int solution = solution("aaaaaaaaaa","bbbbbbbbbb", new String[] {"aaaaaaaaab", "aaaaaaaabb", "aaaaaaabbb", "aaaaaabbbb", "aaaaabbbbb", "aaaabbbbbb", "aaabbbbbbb", "aabbbbbbbb", "abbbbbbbbb",
+				"aaaabaaaaa", "aaaabaaaba", "aaabbaaaab", "babaaabaab", "abbaaaabbb", "bbaabbbbaa", "babaabbbbb", "abbbbbbabb", "babbbbbbbb", "bbabbbbbbb", "bbbabbbbbb"});
+		System.out.println("변환하는 가장 짧은 변환 과정 = "+solution);
 	}
 	public static int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        List<String> changeStr = new ArrayList<>();
-
-        for(int i=0; i<words.length; i++) {
-        	for(int j=0; j<begin.length(); j++) {
-        		StringBuffer word = new StringBuffer(words[i]).deleteCharAt(j);
-        		StringBuffer str = new StringBuffer(begin).deleteCharAt(j);
-        		if(word.toString().equals(str.toString())) {
-        			System.out.println("words[i] = "+words[i]);
-        			changeStr.add(words[i]);
-        		}
-    			System.out.println(i+"번째 "+j+" word = "+word+" str = "+str);
-        	}
-        }
-        if(changeStr.size() > 0) {
-        	changeWordCnt(changeStr);
-        }else {
+		int answer = 0;
+		Queue<String> beginQ = new LinkedList<>();
+		List<String> wordList = new ArrayList<>();
+		if(begin.equals(target)){
             return answer;
         }
-        return answer;
-    }
-	public static int changeWordCnt(List<String> changeStr) {
-		
+		for(String w : words) {
+			wordList.add(w);
+		}
+		for(int i=0; i<wordList.size(); i++) {
+			for(int j=0; j<begin.length(); j++) {
+				StringBuffer word = new StringBuffer(wordList.get(i)).deleteCharAt(j);
+	    		StringBuffer str = new StringBuffer(begin).deleteCharAt(j);
+	    		if(word.toString().equals(str.toString())) {
+	    			beginQ.add(wordList.remove(i));
+	    			j = wordList.size() != 0 ? -1 : j;
+	    		}
+			}
+		}
+		answer++;
+		System.out.println("beginQ = "+beginQ);
+		System.out.println("wordList = "+wordList);
+		int count = beginQ.size();
+		while(beginQ.size() > 0) {
+			if(count == 0) {
+				answer++;
+				count = beginQ.size();
+			}
+			count--;
+//			answer++;
+			System.out.println();
+			System.out.println("answer = "+answer+" count = "+count+" beginQ = "+beginQ);
+			System.out.println();
+			String tmpStr = beginQ.poll();
+			if(tmpStr.equals(target)) {
+				System.out.println("this??? answer = "+answer);
+				return answer; 
+			}
+			for(int i=0; i<wordList.size(); i++) {
+				for(int j=0; j<tmpStr.length(); j++) {
+					StringBuffer word = new StringBuffer(wordList.get(i)).deleteCharAt(j);
+		    		StringBuffer str = new StringBuffer(tmpStr).deleteCharAt(j);
+		    		System.out.println("i = "+i+" j = "+j+" tmpStr = "+tmpStr+" wordList.get(i) = "+wordList.get(i));
+		    		if(word.toString().equals(str.toString())) {
+		    			System.out.println("===같다===");
+		    			beginQ.add(wordList.remove(i));
+		    			System.out.println("size = "+wordList.size());
+		    			if(wordList.size() > 0) {
+		    				j = -1;
+		    			}else {
+		    				break;
+		    			}
+		    		}
+				}
+			}
+		}
+			
 		return 0;
 	}
+	
+//	public static int solution(String begin, String target, String[] words) {
+//        int answer = 0;
+//        
+//        List<String> changeStrList = new ArrayList<>();	// 바꿀 문자열 배열
+//        List<String> remainWord = new ArrayList<>();	// 바뀐 문자열 배열
+//        for(String w : words) {
+//        	changeStrList.add(w);
+//        }
+//        for(int i=0; i<changeStrList.size(); i++) {
+//        	for(int j=0; j<begin.length(); j++) {
+//	    		StringBuffer word = new StringBuffer(changeStrList.get(i)).deleteCharAt(j);
+//	    		StringBuffer str = new StringBuffer(begin).deleteCharAt(j);
+//	    		if(word.toString().equals(str.toString())) {
+//	    			remainWord.add(changeStrList.get(i));
+//	    		}
+//        	}
+//    	}
+//        while(remainWord.size() > 0) {
+//        	for(String r : remainWord) {
+//        		if(changeStrList.indexOf(r) != -1) {
+//            		changeStrList.remove(changeStrList.indexOf(r));
+//            		if(r.equals(target)) {
+//                		return answer;
+//                	}else if(changeStrList.size() == 0) {
+//                		return 0 ;
+//                	}
+//        		}
+//        	}
+//        	String str = remainWord.remove(0);
+//    		
+//    		for(int i=0; i<changeStrList.size(); i++) {
+//            	for(int j=0; j<str.length(); j++) {
+//    	    		StringBuffer word = new StringBuffer(changeStrList.get(i)).deleteCharAt(j);
+//    	    		StringBuffer strBuf = new StringBuffer(str).deleteCharAt(j);
+//    	    		if(word.toString().equals(strBuf.toString())) {
+//    	    			remainWord.add(changeStrList.get(i));
+//    	    		}
+//            	}
+//        	}
+//    		answer++;
+//        }
+//        
+//        return answer;
+//    }
 	
 
 }
---
+++ algorithm01/src/backjun/AplusB.java
@@ -0,0 +1,13 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class AplusB {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a - b);
+	}
+}
